f9be3e0d950db07e0dbc59a7f14f20244ad90047
Larissa <larissa_silva@outlook.com>
2024-12-20 12:41:54 -0300

    first commit

---
+++ src/main/java/itau/case_backend/CaseBackendApplication.java
@@ -0,0 +1,13 @@
+package itau.case_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseBackendApplication.class, args);
+	}
+
+}
