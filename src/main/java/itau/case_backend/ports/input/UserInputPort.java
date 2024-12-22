a4f91a1da8f5455e1ead6588c641c30f82c53a13
Larissa <larissa_silva@outlook.com>
2024-12-21 21:01:16 -0300

    add updatePartialUser

--- src/main/java/itau/case_backend/ports/input/UserInputPort.java
+++ src/main/java/itau/case_backend/ports/input/UserInputPort.java
@@ -1,17 +1,22 @@
 package itau.case_backend.ports.input;
 
+import itau.case_backend.domain.dtos.UserDTO;
+import itau.case_backend.domain.dtos.UserPartialUpdateDTO;
 import itau.case_backend.domain.entities.User;
 import java.util.List;
 
 /**
  * Define os métodos de entrada para operações relacionadas a usuários.
+ * <p>
+ * Esta interface especifica os métodos que são usados para buscar, criar, atualizar e excluir usuários no sistema.
+ * </p>
  */
 public interface UserInputPort {
 
     /**
      * Busca todos os usuários cadastrados.
      *
-     * @return uma lista de usuários
+     * @return uma lista de {@link User} contendo todos os usuários
      */
     List<User> getAllUsers();
 
@@ -19,26 +24,36 @@ public interface UserInputPort {
      * Busca um usuário pelo ID.
      *
      * @param id o identificador único do usuário
-     * @return o usuário correspondente ao ID, ou null se não encontrado
+     * @return o usuário correspondente ao ID, ou {@code null} se não encontrado
      */
     User getUserById(long id);
 
     /**
-     * Cria um novo usuário.
+     * Cria um novo usuário com base nos dados fornecidos.
      *
-     * @param user o objeto contendo os dados do novo usuário
-     * @return o usuário criado com seu ID gerado
+     * @param userDTO o objeto {@link UserDTO} contendo os dados necessários para criar o usuário
+     * @return o {@link User} criado com o ID gerado e outros dados persistidos
+     * @throws IllegalArgumentException se os dados do usuário forem inválidos
      */
-    User createUser(User user);
+    User createUser(UserDTO userDTO);
 
     /**
      * Atualiza os dados de um usuário existente.
      *
      * @param id o identificador único do usuário
-     * @param user o objeto contendo os novos dados do usuário
-     * @return o usuário atualizado
+     * @param userDTO o objeto {@link UserDTO} contendo os novos dados do usuário
+     * @return o {@link User} atualizado
      */
-    User updateUser(long id, User user);
+    User updateUser(long id, UserDTO userDTO);
+
+    /**
+     * Atualiza parcialmente os dados de um usuário existente.
+     *
+     * @param id o identificador único do usuário
+     * @param updatedUserDTO o objeto {@link UserPartialUpdateDTO} contendo os dados atualizados
+     * @return o {@link User} atualizado com os dados parciais
+     */
+    User updatePartialUser(long id, UserPartialUpdateDTO updatedUserDTO);
 
     /**
      * Remove um usuário pelo ID.
