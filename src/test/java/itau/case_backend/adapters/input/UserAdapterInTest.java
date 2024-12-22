3c90781bf8e55b4ea65ce1460978e3e382890074
Larissa <larissa_silva@outlook.com>
2024-12-22 01:20:29 -0300

    Add and update tests

--- src/test/java/itau/case_backend/adapters/input/UserAdapterInTest.java
+++ src/test/java/itau/case_backend/adapters/input/UserAdapterInTest.java
@@ -1,5 +1,9 @@
 package itau.case_backend.adapters.input;
 
+import itau.case_backend.adapters.output.UserRepository;
+import itau.case_backend.config.exception.EmailAlreadyExistsException;
+import itau.case_backend.config.exception.UserNotFoundException;
+import itau.case_backend.domain.dtos.UserDTO;
 import itau.case_backend.domain.entities.User;
 import itau.case_backend.ports.input.UserInputPort;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,9 +11,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -26,24 +32,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class UserAdapterInTest {
 
+    @Autowired
     private MockMvc mockMvc;
 
-    @Mock
+    @MockitoBean
     private UserInputPort userInputPort;
-
-    @InjectMocks
-    private UserAdapterIn userAdapterIn;
-
-    @BeforeEach
-    void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(userAdapterIn).build();
-    }
 
     @Test
     void testGetAllUsers() throws Exception {
         when(userInputPort.getAllUsers()).thenReturn(List.of(new User(1, "John Doe", "john.doe@example.com", 25)));
 
-        mockMvc.perform(get("/user"))
+        mockMvc.perform(get("/users"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].name").value("John Doe"))
                 .andExpect(jsonPath("$[0].email").value("john.doe@example.com"))
@@ -54,7 +53,7 @@ class UserAdapterInTest {
     void testGetUserById_UserFound() throws Exception {
         when(userInputPort.getUserById(1)).thenReturn(new User(1, "John Doe", "john.doe@example.com", 25));
 
-        mockMvc.perform(get("/user/1"))
+        mockMvc.perform(get("/users/1"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name").value("John Doe"))
                 .andExpect(jsonPath("$.email").value("john.doe@example.com"))
@@ -65,7 +64,7 @@ class UserAdapterInTest {
     void testGetUserById_UserNotFound() throws Exception {
         when(userInputPort.getUserById(1)).thenReturn(null);
 
-        mockMvc.perform(get("/user/1"))
+        mockMvc.perform(get("/users/1"))
                 .andExpect(status().isNotFound())
                 .andExpect(content().string("Usuário não encontrado."));
     }
@@ -73,24 +72,28 @@ class UserAdapterInTest {
     @Test
     void testCreateUser() throws Exception {
         User newUser = new User(1, "John Doe", "john.doe@example.com", 25);
-        when(userInputPort.createUser(any(User.class))).thenReturn(newUser);
+        UserDTO userDTO = new UserDTO("John Doe", "john.doe@example.com", 25);
 
-        mockMvc.perform(post("/user")
+        when(userInputPort.createUser(any(UserDTO.class))).thenReturn(newUser);
+
+        mockMvc.perform(post("/users")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"name\":\"John Doe\",\"email\":\"john.doe@example.com\",\"age\":25}"))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.name").value("John Doe"))
                 .andExpect(jsonPath("$.email").value("john.doe@example.com"))
                 .andExpect(jsonPath("$.age").value(25));
+
+        verify(userInputPort, times(1)).createUser(any(UserDTO.class));
     }
 
     @Test
     void testUpdateUser() throws Exception {
         User updatedUser = new User(1, "John Smith", "john.smith@example.com", 26);
 
-        when(userInputPort.updateUser(eq(1L), any(User.class))).thenReturn(updatedUser);
+        when(userInputPort.updateUser(eq(1L), any(UserDTO.class))).thenReturn(updatedUser);
 
-        mockMvc.perform(put("/user/1")
+        mockMvc.perform(put("/users/1")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"name\":\"John Smith\",\"email\":\"john.smith@example.com\",\"age\":26}"))
                 .andExpect(status().isOk())
@@ -100,16 +103,34 @@ class UserAdapterInTest {
     }
 
     @Test
-    void testUpdateUser_NotFound() throws Exception {
-        User updatedUser = new User(1, "John Smith", "john.smith@example.com", 26);
-        when(userInputPort.updateUser(1, updatedUser)).thenReturn(null);
+    void testUpdateUser_EmailAlreadyExists() throws Exception {
+        UserDTO userDTO = new UserDTO("John Smith", "smith.doe@example.com", 26);
 
-        mockMvc.perform(put("/user/1")
+        when(userInputPort.updateUser(eq(1L), any(UserDTO.class)))
+                .thenThrow(new EmailAlreadyExistsException(userDTO.getEmail()));
+
+        mockMvc.perform(put("/users/1")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"name\":\"John Smith\",\"email\":\"john.smith@example.com\",\"age\":26}"))
-                .andExpect(status().isNotFound())
-                .andExpect(content().string("Usuário não encontrado."));
+                        .content("{\"name\":\"John Smith\",\"email\":\"smith.doe@example.com\",\"age\":26}"))
+                .andExpect(status().isConflict())  // Espera o status HTTP 409
+                .andExpect(jsonPath("$.errors[0]").value("O e-mail fornecido (smith.doe@example.com) já está cadastrado em nosso sistema. Por favor, insira um e-mail diferente."));
     }
+
+    @Test
+    void testUpdateUser_UserNotFound() throws Exception {
+        UserDTO userDTO = new UserDTO("John Smith", "smith.doe@example.com", 26);
+
+        when(userInputPort.updateUser(eq(1L), any(UserDTO.class)))
+                .thenThrow(new UserNotFoundException(userDTO.toEntity().getId()));
+
+        mockMvc.perform(put("/users/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\":\"John Smith\",\"email\":\"smith.doe@example.com\",\"age\":26}"))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.errors[0]").value("Usuário com id 0 não encontrado."));
+    }
+
+
 
     @Test
     void testDeleteUser() throws Exception {
@@ -118,6 +139,64 @@ class UserAdapterInTest {
 
         verify(userInputPort, times(1)).deleteUser(1);
     }
+
+
+    @Test
+    public void whenNameIsBlank_thenReturnValidationError() throws Exception {
+        String invalidUser = "{\"name\": \"\", \"email\": \"valid@example.com\", \"age\": 25}";
+
+        mockMvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(invalidUser))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors").value("O nome não pode estar vazio; "));
+    }
+
+    @Test
+    public void whenEmailIsInvalid_thenReturnValidationError() throws Exception {
+        String invalidUser = "{\"name\": \"John\", \"email\": \"invalid-email\", \"age\": 25}";
+
+        mockMvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(invalidUser))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors").value("Formato de e-mail inválido"));
+    }
+
+    @Test
+    public void whenAgeIsLessThanOne_thenReturnValidationError() throws Exception {
+        String invalidUser = "{\"name\": \"John\", \"email\": \"john@example.com\", \"age\": 0}";
+
+        mockMvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(invalidUser))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors").value("A idade deve ser maior que 0"));
+    }
+
+    @Test
+    public void whenAgeIsNull_thenReturnValidationError() throws Exception {
+        String invalidUser = "{\"name\": \"John\", \"email\": \"john@example.com\", \"age\": null}";
+
+        mockMvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(invalidUser))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors").value("A idade não pode ser nula"));
+    }
+
+
+    @Test
+    public void whenPartialUpdateWithInvalidEmail_thenReturnValidationError() throws Exception {
+        String partialUpdate = "{\"email\": \"invalidemail\"}"; // Invalid email
+
+        mockMvc.perform(patch("/users/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(partialUpdate))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors").value("Formato de e-mail inválido; "));
+    }
+
 
 
 }
